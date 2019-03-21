0b4e455b35054d8cf80c275f754865ad0bcf1031
Malaika Handa <mhanda@andrew.cmu.edu>
2019-03-20 23:54:10 -0400

    updates to plots

--- plots/plots.r
+++ plots/plots.r
@@ -5,6 +5,7 @@ library(tidyverse)
 # data
 df = read_csv("pixels.csv", col_types = cols()) %>%
   mutate(col = rgb(r_mean, g_mean, b_mean, maxColorValue = 255))
+#       (col = rgb(r_mean, g_mean, b_mean, maxColorValue = 255))
 
 # plot
 
@@ -12,7 +13,8 @@ palette = df$col
 names(palette) = df$col
 
 ggplot(data = df,
-       aes(x = b_mean - (r_mean + g_mean),
+       aes(x = b_mean,
+#          x = b_mean + (r_mean + g_mean)
            y = seed,
            label = team)) +
   geom_point(aes(color = col),
@@ -20,10 +22,10 @@ ggplot(data = df,
   scale_color_manual(values = palette) +
   geom_text(color = "white",
             size = 3) +
-  labs(title = "Bluest NCAA Teams",
-       x = "Average Blueness of Logo (penalized)",
+  labs(title = "Bluest NCAA Teams (Mean)",
+       x = "b)",
        y = "Seed") +
   theme_bw() +
   theme(legend.position = "none")
 
-# ggsave("bluepen.jpg", width = 14, height = 8, units = "in")
+ggsave("plots/mean.jpg", width = 14, height = 6, units = "in")
